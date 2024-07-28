bef6facb921c05ff5afa523a586f6614abbf0faf
AndreiListapp <andrei.carmici@listapp.eu>
2024-07-28 04:57:59 +0300

    fixes

--- hello-word-client/next.config.js
+++ hello-word-client/next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    outputStandalone: true,
+  },
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve = {
+        ...config.resolve,
+        fallback: {
+          ...config.resolve.fallback,
+          fs: false,
+        },
+      };
+    }
+    config.module = {
+      ...config.module,
+      exprContextCritical: false,
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
